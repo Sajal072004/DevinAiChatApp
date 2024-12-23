21685d4ab5dcf79abf24c0e7daab780aadc75e61
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-12-23 15:26:30 +0530

    feat: add JWT authentication and password hashing to user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import 'dotenv/config.js';
 
 const userSchema = new mongoose.Schema({
 
@@ -16,8 +19,22 @@ const userSchema = new mongoose.Schema({
     type: String,
     minLength: [6, "password must be at least 6 characters"],
     maxLength: [50, "password must be at most 50 characters"],
+    select:false,
   }
 
  })
 
- 
+userSchema.statics.hashPassword = async function(password) {
+  return await bcrypt.hash(password, 10);
+}
+
+userSchema.methods.isValidPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateJWT = function() {
+  return jwt.sign({email: this.email}, process.env.JWT_SECRET);
+}
+
+const User = mongoose.model('User', userSchema);
+export default User;
